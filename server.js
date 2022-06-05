72c49afa75d81fe38faa15bbe5986645566a8ca1
Edidee <etokedidionge@gmail.com>
2022-06-05 16:31:43 +0100

    omo

--- server.js
+++ server.js
@@ -18,7 +18,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "..", "school-client/public");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
