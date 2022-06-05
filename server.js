80cc744f8bfb88e7d8ecd7fb6de1fc43e7c15c4e
Edidee <etokedidionge@gmail.com>
2022-06-05 15:39:10 +0100

    still trying

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+// import express from "express";
+// import path from "path";
 
-// const path = require("path");
-// const express = require("express");
 // const app = express();
-// const publicPath = path.join(__dirname, "..", "public");
-// app.use(express.static(publicPath));
+
+// const __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
 app.listen(port, () => {
-  console.log("Server is up!");
+  console.log(`Server is up on port ${port}!`);
 });
